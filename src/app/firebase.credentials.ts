4477e985e422c4748705ed8f0093b304284b8a02
Maikon Albuquerque <maikonmorais@gmail.com>
2018-12-20 13:11:34 -0300

    Adding firebase configs and button to AddShoppingItemPage

---
+++ src/app/firebase.credentials.ts
@@ -0,0 +1,8 @@
+export const FIREBASE_CONFIG = {
+  apiKey: "AIzaSyCOb3hYuLBRcDQyqToroaVgFrNIYXWCpBE",
+  authDomain: "shoppinglist-c10e6.firebaseapp.com",
+  databaseURL: "https://shoppinglist-c10e6.firebaseio.com",
+  projectId: "shoppinglist-c10e6",
+  storageBucket: "shoppinglist-c10e6.appspot.com",
+  messagingSenderId: "227672221428"
+};
