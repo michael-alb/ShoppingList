d48a2123592d64670915bd8d74b1cc4bb5cbcc0a
Maikon Albuquerque <maikonmorais@gmail.com>
2018-12-20 16:37:27 -0300

    Listing added objects in the addShoppingItem

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,9 @@
+import { Observable } from 'rxjs';
+import 'rxjs/add/operator/map';
+import { ShoppingListService } from './../../services/shopping-list/shopping-list';
 import { Component } from '@angular/core';
 import { NavController, IonicPage } from 'ionic-angular';
+import { Item } from '../../models/item/item.model';
 
 @IonicPage()
 @Component({
@@ -8,7 +12,20 @@ import { NavController, IonicPage } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  shoppingList$: Observable<Item[]>
+
+  constructor(public navCtrl: NavController, private shoppingListService: ShoppingListService) {
+
+    this.shoppingList$ = this.shoppingListService
+      .getShoppingList()
+      .snapshotChanges()
+      .map(
+        changes => {
+          return changes.map( c => ({
+            key: c.payload.key, ...c.payload.val()
+          }))
+        }
+      )
 
   }
 
